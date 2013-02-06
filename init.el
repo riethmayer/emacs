df21ed4c487b45fd5c350dcfaf7dfab50e716238
Jan Riethmayer <jan@riethmayer.de>
2013-02-07 00:33:11 +0100

    key binds

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (setq inhibit-startup-message t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode t)
-(scroll-bar-mode 0)
+(scroll-bar-mode -1)
 (blink-cursor-mode t)
 (show-paren-mode t)
 (set-terminal-coding-system 'utf-8)
@@ -18,6 +18,30 @@
 (global-subword-mode 1)
 (menu-bar-mode 0)
 (tool-bar-mode -1)
+
+;; my keys
+
+(setq mac-option-key-is-meta nil
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier 'super
+      mac-pass-command-to-system nil)
+
+(global-set-key (kbd "C-x \\") 'align-regexp)
+(global-set-key "\C-s" 'isearch-forward-regexp)
+(global-set-key "\C-r" 'isearch-backward-regexp)
+(global-set-key "\C-\M-s" 'isearch-forward)
+(global-set-key "\C-\M-r" 'isearch-backward)
+(global-set-key (kbd "C-x f") 'recentf-open-files)
+(global-set-key (kbd "C-x C-p") 'find-file-at-point)
+(global-set-key (kbd "C-c y") 'bury-buffer)
+(global-set-key (kbd "M-`") 'file-cache-minibuffer-complete)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-n") 'ns-toggle-fullscreen)
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
+(setq visible-bell 1)
 
 ;; my functions
 
@@ -34,6 +58,8 @@
   (untabify (point-min) (point-max)))
 (global-set-key (kbd "C-c n") 'indent-buffer)
 
+;; my modes
+(recentf-mode)
 
 ;; start server and fullscreen
 (run-with-idle-timer 0.1 nil 'ns-toggle-fullscreen)
