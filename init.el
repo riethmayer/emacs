a396e84da9e6573d2cb36c8cc85b7ec2df47049e
Jan Riethmayer <jan@riethmayer.de>
2013-07-31 10:32:27 +0200

    load zenburn theme :)

--- init.el
+++ init.el
@@ -165,7 +165,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
+ '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
  '(debug-on-error t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -174,3 +174,4 @@
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "Magenta" :foreground "Black"))))
  '(hl-line ((t (:inherit highlight :foreground "Black" :background "white")))))
+(load-theme 'zenburn)
