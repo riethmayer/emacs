d2aa67c83c3110710d7c952ca04633cde104cd5c
Jan Riethmayer <jan@riethmayer.de>
2013-09-02 19:32:18 +0200

    try out monkai-theme.

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
                       coffee-mode
                       zenburn-theme
                       dash-at-point
+                      monokai-theme
                       yasnippet)
   "A list of packages to ensure are installed at launch.")
 
@@ -136,7 +137,7 @@
 
 ;; cocoa specifics
 (when (memq window-system '(mac ns))
-  (set-face-attribute 'default nil :font "Menlo-18")
+  (set-face-attribute 'default nil :font "Menlo-22")
   (run-with-idle-timer 0.1 nil 'ns-toggle-fullscreen)
   (exec-path-from-shell-initialize))
 
@@ -174,7 +175,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
+ '(custom-safe-themes (quote ("fa189fcf5074d4964f0a53f58d17c7e360bb8f879bd968ec4a56dc36b0013d29" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "9f443833deb3412a34d2d2c912247349d4bd1b09e0f5eaba11a3ea7872892000" default)))
  '(debug-on-error t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -183,3 +184,4 @@
  ;; If there is more than one, they won't work right.
  '(cursor ((t (:background "Magenta" :foreground "Black"))))
  '(hl-line ((t (:inherit highlight :foreground "Black" :background "white")))))
+(load-theme 'monokai)
