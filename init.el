c7663de3c59202510ed1bd7c6c2c7cfc00a7b66f
Jan Riethmayer <jan@riethmayer.de>
2015-05-08 17:58:19 +0100

    use melpa only

--- init.el
+++ init.el
@@ -1,13 +1,10 @@
 ;; package system
 ;; see https://github.com/technomancy/emacs-starter-kit
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
 
-;; my packages
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize) ;; You might already have this line
 
 (when (not package-archive-contents)
   (package-refresh-contents))
